87df6b8728e0234cbc66bc8ef8a54565ca916c3f
starryrbs <1322096624@qq.com>
2019-08-20 23:06:56 +0800

    完成了第一个单元测试，url mapping和视图

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 
 class SmokeTest(TestCase):
     def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+        self.assertEqual(1 + 1, 2)
+
+    def test_resolve_root_url_to_home_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
